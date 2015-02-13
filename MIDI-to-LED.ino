4b47acd9bc09db3a3f2ec4bc30da468f2f83f4e3
Rytis Bizauskas <luxapto@gmail.com>
2015-02-12 19:46:11 -0600

    Revert 51f2789..b8738ca
    This rolls back to commit 51f27897e043b14fa25b5613196a81a2fb1a1592.

--- MIDI-to-LED.ino
+++ MIDI-to-LED.ino
@@ -1,83 +1,113 @@
 #include <Adafruit_NeoPixel.h>
 
-// #define PIN            12
-// #define NUMPIXELS      36
-// Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(24, 12, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel pixels2 = Adafruit_NeoPixel(12, 7, NEO_GRB + NEO_KHZ800);
-
+#define PIN            12
+#define NUMPIXELS      36
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 byte incomingByte;
 
+//RGB variables
 int red = 5;
 int green = 0;
 int blue = 5;
 
+//Light control variables
 int delay1 = 20;
 int delay2 = 750;
 int delay3 = 0;
 int increment = 0;
 int action = 0;
 
-boolean noteOn = false; 
+//Status Bytes
+boolean noteOn = false;
 boolean controlChange = false;
 
+//Second Byte
 int note = 0;
-int controlType = 0;
+int controlFunction = 0;
 
+//Third Byte
 int velocity = 0;
 int controlValue = 0;
 
 void setup() {
+  //Intialize serial/MIDI com port and NeoPixels
   Serial.begin(115200);
   pixels.begin(); 
-  pixels2.begin(); 
 }
 
 void loop() {
   if(Serial.available() > 0) {
 
-    incomingByte = Serial.read();    
+    incomingByte = Serial.read();
     
-    if (incomingByte == 144){        
-      noteOn = true;                 
+    if (incomingByte == 144){
+      noteOn = true;
     }
-      else if (incomingByte == 128){    
+      else if (incomingByte == 128){
         noteOn = false;
       }
         else if (incomingByte == 176){
           controlChange = true;
         }
           else if (controlChange == true){
-            controlType = incomingByte;
+            controlFunction = incomingByte;
             delay1 = 1000;
           }
-            else if (controlType == 17){
+            else if (controlFunction == 17){
               controlValue = incomingByte;
               increment = controlValue - '0'; 
             }
-              else if (noteOn == false && note == 0){ 
+              else if (noteOn == false && note == 0){ // if we received a "note off", we wait for which note (databyte)      
                 clearLights();
+                
                 note = incomingByte;
                 note = 0;
                 velocity = 0;
               }
-                else if (noteOn == true && note == 0){ 
+                else if (noteOn == true && note == 0){ // if we received a "note on", we wait for the note (databyte)
                   pulseLights();
                   // sendLights();
                   note=incomingByte;
                 } 
-                  else if (noteOn == true && note != 0){ 
+                  else if (noteOn == true && note != 0){ // ...and then the velocity
                     velocity=incomingByte;
+                    //playNote(note, velocity);
                     note=0;
                     velocity=0;
                     action=0;
                   }
-    else{
-    }    
+                    else{
+                    }    
   }
 }
+
+    
+//    if(incomingByte == 176)
+//    {
+//      controlChange = true;
+//    }
+//    if(incomingByte == 11)
+//    {
+//      knob1Trigger = true; 
+//    }
+//    if(controlChange == true && knob1Trigger == true)
+//    {
+//      increment = incomingByte- '0';
+//    }
+//    
+////    else if(controlChange == true)
+////    {
+////      increment = Serial.read();
+////      controlChange = false;
+////    }
+//            
+//    if (incomingByte == 144) {
+//      pulseLights();
+//    }
+//    else if(incomingByte == 128) {
+//      clearLights();
+//    }
 
 void clearLights() {
   for(int i=0;i<36;i++){
@@ -86,7 +116,9 @@ void clearLights() {
   pixels.show(); 
 }
 
+
 void pulseLights() {
+  
   int loops = 0;
   while (loops < 20)
   {
@@ -94,11 +126,9 @@ void pulseLights() {
     blue = blue + 2;
     for(int i=0; i<36; i++)
     {
-      pixels.setPixelColor(i, pixels.Color(red, green, blue));
-      pixels2.setPixelColor(i, pixels.Color(red, green, blue));
+      pixels.setPixelColor(i, pixels.Color(red, green, blue));        
     }
     pixels.show();
-    pixels2.show();
     delay(5);
     loops++;
   }
@@ -107,11 +137,29 @@ void pulseLights() {
 }
 
 void sendLights() {
+
 // changeColor();
-  for(int i=0;i<36;i++){
-    pixels.setPixelColor(i, pixels.Color(red, green, blue));
+int loops = 0;
+while (loops < 20)
+{
+  red = red + 2;
+  blue = blue + 2;
+  for(int i=0; i<36; i++)
+  {
+    pixels.setPixelColor(i, pixels.Color(red, green, blue));        
   }
   pixels.show();
+  delay(100);
+  loops++;
+}
+// for(int i=0;i<36;i++){
+//           pixels.setPixelColor(i, pixels.Color(red, green, blue));
+// //          delay(5);
+// //          pixels.show();
+//         }
+//         pixels.show();
+
+
 }
 
 void changeColor()
