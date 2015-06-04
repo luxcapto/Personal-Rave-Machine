a7ef79634f7d51988ec5f702d31b50317401ac0f
Nash Kaminski <nashkaminski@comcast.net>
2015-06-03 22:35:51 -0400

    add basic scheduler, fix curly brace

--- example_evsched.c
+++ example_evsched.c
@@ -3,31 +3,32 @@ int counter1 = 0;
 char running1 = 0;
 
 void setup(){
-long pmillis = millis();
+        long pmillis = millis();
 }
 
 void loop(){
-ev_sched()
+        ev_sched()
 }
 
 
 void ev_sched{
-    if( (millis()-pmillis) >= interval){
-    //this runs periodically
-    if(running1){
-        switch(counter1){
+        if( (millis()-pmillis) >= interval){
+                //this runs periodically
+                if(running1){
+                        switch(counter1){
 
-        case 1:
-           //first step in animation
-           break;
-        case 2:
-            //2nd step and so forth
-            //.....
-            break;
-        case 100:
-            running1=0;
-            counter1=0;
-       }
-
-    }   
+                                case 1:
+                                        //first step in animation
+                                        break;
+                                case 2:
+                                        //2nd step and so forth
+                                        //.....
+                                        break;
+                                case 100:
+                                        running1=0;
+                                        counter1=0;
+                        }
+                        counter1++;
+                }
+        }   
 }
