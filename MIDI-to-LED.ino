72a7a86142096afc605a306b659ec21bb36ff85f
Rytis Bizauskas <luxcapto@gmail.com>
2015-06-13 10:40:52 -0500

    Fixed CC lighting when note off

--- MIDI-to-LED.ino
+++ MIDI-to-LED.ino
@@ -24,9 +24,10 @@ int upperStripGreen = 0;
 byte noteReceived = 0;
 byte noteOffReceived = 0;
 bool off = false;
+bool cc = false;
 
 void setup() {
-//  Serial.begin(9600);
+ // Serial.begin(9600);
   lowerStrip.begin();
   upperStrip.begin();
 
@@ -52,12 +53,15 @@ void onNoteOn(byte channel, byte note, byte velocity) {
     lowerStripFirst(lowerStripRed, lowerStripGreen, lowerStripBlue);
   } else if (note == 56) {
     lowerStripSecond(lowerStripRed, lowerStripGreen, lowerStripBlue);
+  } else if (note == 57) {
+    lowerStripThird(lowerStripRed, lowerStripGreen, lowerStripBlue);
   }
   off = false;
   noteReceived = note;
 }
 
 void onNoteOff(byte channel, byte note, byte velocity) {
+  Serial.write("Note off received");
   if (note == 54) {
     clearLowerStrip();
   } else if (note == 46) {
@@ -66,61 +70,72 @@ void onNoteOff(byte channel, byte note, byte velocity) {
     clearLowerStrip();
   } else if (note == 56) {
     clearLowerStrip();
-  }
+  } else if (note == 57) {
+    clearLowerStrip();
+  } 
   off = true;
   noteOffReceived = note;
 }
 
 void onControlChange(byte channel, byte controlType, byte value) {
-  if(controlType == 26) {
-    if(noteReceived == 54) {
-      lowerStripRed = (int)value*2;
-      lowerStripOn(lowerStripRed, lowerStripGreen, lowerStripBlue);
-      // upAndDown (lowerStripRed, lowerStripGreen, lowerStripBlue, (int)value);
-    } else {
+  if(!off) {
+    if(controlType == 26) {
+      if(noteReceived == 54) {
         lowerStripRed = (int)value*2;
-    }
-  } else if (controlType == 27) {
-      if(noteReceived == 54) {
-        lowerStripGreen = (int)value*2;
         lowerStripOn(lowerStripRed, lowerStripGreen, lowerStripBlue);
+        // upAndDown (lowerStripRed, lowerStripGreen, lowerStripBlue, (int)value);
       } else {
+          lowerStripRed = (int)value*2;
+      }
+    } else if (controlType == 27) {
+        if(noteReceived == 54) {
           lowerStripGreen = (int)value*2;
-      }
-  } else if (controlType == 28) {
-      if(noteReceived == 54) {
-        lowerStripBlue = (int)value*2;
-        lowerStripOn(lowerStripRed, lowerStripGreen, lowerStripBlue);
-      } else {
+          lowerStripOn(lowerStripRed, lowerStripGreen, lowerStripBlue);
+        } else {
+            lowerStripGreen = (int)value*2;
+        }
+    } else if (controlType == 28) {
+        if(noteReceived == 54) {
           lowerStripBlue = (int)value*2;
-      }
-  } else if(controlType == 23) {
-      if(noteReceived == 46) {
-        upperStripRed = (int)value*2;
-        upperStripOn(upperStripRed, upperStripGreen, upperStripBlue);
-      } else {
+          lowerStripOn(lowerStripRed, lowerStripGreen, lowerStripBlue);
+        } else {
+            lowerStripBlue = (int)value*2;
+        }
+    } else if(controlType == 23) {
+        if(noteReceived == 46) {
           upperStripRed = (int)value*2;
-      }
-  } else if (controlType == 24) {
-      if(noteReceived == 46) {
-        upperStripGreen = (int)value*2;
-        upperStripOn(upperStripRed, upperStripGreen, upperStripBlue);
-      } else {
+          upperStripOn(upperStripRed, upperStripGreen, upperStripBlue);
+        } else {
+            upperStripRed = (int)value*2;
+        }
+    } else if (controlType == 24) {
+        if(noteReceived == 46) {
           upperStripGreen = (int)value*2;
-      }
-  } else if (controlType == 25) {
-      if(noteReceived == 46) {
-        upperStripBlue = (int)value*2;
-        upperStripOn(upperStripRed, upperStripGreen, upperStripBlue);
-      } else {
+          upperStripOn(upperStripRed, upperStripGreen, upperStripBlue);
+        } else {
+            upperStripGreen = (int)value*2;
+        }
+    } else if (controlType == 25) {
+        if(noteReceived == 46) {
           upperStripBlue = (int)value*2;
-      }
+          upperStripOn(upperStripRed, upperStripGreen, upperStripBlue);
+        } else {
+            upperStripBlue = (int)value*2;
+        }
+    }
   }
 }
 
 void stripsOn(int red, int green, int blue) {
   lowerStripOn(red, green, blue);
   upperStripOn(red, green, blue);
+}
+
+void stripsOff() {
+  lowerStripOn(0, 0, 0);
+  upperStripOn(0, 0, 0);
+  lowerStrip.show();
+  upperStrip.show();
 }
 
 void lowerStripFirst(int red, int green, int blue) {
@@ -136,7 +151,19 @@ void lowerStripFirst(int red, int green, int blue) {
 }
 
 void lowerStripSecond(int red, int green, int blue) {
-  for (int i=12;i<23;i++) {
+  for (int i=11;i<22;i++) {
+    lowerStrip.setPixelColor(i, lowerStrip.Color(red, green, blue));
+  }
+
+  lowerStrip.show();
+
+  lowerStripRed = red;
+  lowerStripGreen = green;
+  lowerStripBlue = blue;
+}
+
+void lowerStripThird(int red, int green, int blue) {
+  for (int i=22;i<33;i++) {
     lowerStrip.setPixelColor(i, lowerStrip.Color(red, green, blue));
   }
 
