1159793f64c9a6dfbd7a3f7827364c90c4e52429
Rytis Bizauskas <luxcapto@gmail.com>
2015-07-30 17:01:26 -0500

    Revert 2988218..c6c9222
    This rolls back to commit 29882182f3a0aeec387d8a6c9a82207cd340eb10.

--- MIDI-to-LED.ino
+++ MIDI-to-LED.ino
@@ -1,3 +1,6 @@
+// 
+
+
 #include <Adafruit_NeoPixel.h>
 #include <avr/power.h>
 
@@ -18,15 +21,12 @@ int upperStripRed = 0;
 int upperStripBlue = 0;
 int upperStripGreen = 0;
 
-int lastCC = 0;
-
 byte noteReceived = 0;
 byte noteOffReceived = 0;
 bool off = false;
-bool cc = false;
 
 void setup() {
- Serial.begin(9600);
+//  Serial.begin(9600);
   lowerStrip.begin();
   upperStrip.begin();
 
@@ -34,19 +34,9 @@ void setup() {
   usbMIDI.setHandleNoteOn(onNoteOn);
   usbMIDI.setHandleControlChange(onControlChange);
 
- for(int i=0;i<LOWER_STRIP_PIXELS;i++) {
-      lowerStrip.setPixelColor(i, lowerStrip.Color(255, 0, 0));
-      upperStrip.setPixelColor(i, upperStrip.Color(255, 0, 0));
-
-      delay(5);
-      lowerStrip.show(); 
-      upperStrip.show();
-  } 
-  delay(500);
-
-  // stripsOn(55,55,55);
-  // delay(250);
-   clearStrips();
+  stripsOn(55,55,55);
+  delay(2500);
+  clearStrips();
 }
 
 void loop() {
@@ -55,91 +45,76 @@ void loop() {
 
 void onNoteOn(byte channel, byte note, byte velocity) {
   if (note == 54) {
-    panOut(lowerStripRed, lowerStripGreen, lowerStripBlue, lastCC);
+    lowerStripOn(lowerStripRed, lowerStripGreen, lowerStripBlue);
   } else if (note == 46) {
     upperStripOn(upperStripRed, upperStripGreen, upperStripBlue);
   } else if (note == 55) {
     lowerStripFirst(lowerStripRed, lowerStripGreen, lowerStripBlue);
-<<<<<<< HEAD
   } else if (note == 56) {
     lowerStripSecond(lowerStripRed, lowerStripGreen, lowerStripBlue);
-  } else if (note == 57) {
-    lowerStripThird(lowerStripRed, lowerStripGreen, lowerStripBlue);
-=======
->>>>>>> parent of 2988218... Pitch commit
   }
   off = false;
   noteReceived = note;
 }
 
 void onNoteOff(byte channel, byte note, byte velocity) {
-  // Serial.write("Note off received");
   if (note == 54) {
     clearLowerStrip();
   } else if (note == 46) {
     clearUpperStrip();
   } else if (note == 55) {
     clearLowerStrip();
-<<<<<<< HEAD
   } else if (note == 56) {
     clearLowerStrip();
-  } else if (note == 57) {
-    clearLowerStrip();
-  } 
-=======
   }
->>>>>>> parent of 2988218... Pitch commit
   off = true;
   noteOffReceived = note;
 }
 
 void onControlChange(byte channel, byte controlType, byte value) {
-  if(!off) {
-    if(controlType == 26) {
-      if(noteReceived == 54) {
+  if(controlType == 26) {
+    if(noteReceived == 54) {
+      lowerStripRed = (int)value*2;
+      lowerStripOn(lowerStripRed, lowerStripGreen, lowerStripBlue);
+      // upAndDown (lowerStripRed, lowerStripGreen, lowerStripBlue, (int)value);
+    } else {
         lowerStripRed = (int)value*2;
-        // lowerStripOn(lowerStripRed, lowerStripGreen, lowerStripBlue);
-        panOut (lowerStripRed, lowerStripGreen, lowerStripBlue, (int)value);
-        lastCC = (int)value;
-      } else {
-          lowerStripRed = (int)value*2;
-      }
-    } else if (controlType == 27) {
-        if(noteReceived == 54) {
-          lowerStripGreen = (int)value*2;
-          lowerStripOn(lowerStripRed, lowerStripGreen, lowerStripBlue);
-        } else {
-            lowerStripGreen = (int)value*2;
-        }
-    } else if (controlType == 28) {
-        if(noteReceived == 54) {
-          lowerStripBlue = (int)value*2;
-          lowerStripOn(lowerStripRed, lowerStripGreen, lowerStripBlue);
-        } else {
-            lowerStripBlue = (int)value*2;
-        }
-    } else if(controlType == 23) {
-        if(noteReceived == 46) {
-          upperStripRed = (int)value*2;
-          upperStripOn(upperStripRed, upperStripGreen, upperStripBlue);
-        } else {
-            upperStripRed = (int)value*2;
-        }
-    } else if (controlType == 24) {
-        if(noteReceived == 46) {
-          upperStripGreen = (int)value*2;
-          upperStripOn(upperStripRed, upperStripGreen, upperStripBlue);
-        } else {
-            upperStripGreen = (int)value*2;
-        }
-    } else if (controlType == 25) {
-        if(noteReceived == 46) {
-          upperStripBlue = (int)value*2;
-          upperStripOn(upperStripRed, upperStripGreen, upperStripBlue);
-        } else {
-            upperStripBlue = (int)value*2;
-        }
     }
+  } else if (controlType == 27) {
+      if(noteReceived == 54) {
+        lowerStripGreen = (int)value*2;
+        lowerStripOn(lowerStripRed, lowerStripGreen, lowerStripBlue);
+      } else {
+          lowerStripGreen = (int)value*2;
+      }
+  } else if (controlType == 28) {
+      if(noteReceived == 54) {
+        lowerStripBlue = (int)value*2;
+        lowerStripOn(lowerStripRed, lowerStripGreen, lowerStripBlue);
+      } else {
+          lowerStripBlue = (int)value*2;
+      }
+  } else if(controlType == 23) {
+      if(noteReceived == 46) {
+        upperStripRed = (int)value*2;
+        upperStripOn(upperStripRed, upperStripGreen, upperStripBlue);
+      } else {
+          upperStripRed = (int)value*2;
+      }
+  } else if (controlType == 24) {
+      if(noteReceived == 46) {
+        upperStripGreen = (int)value*2;
+        upperStripOn(upperStripRed, upperStripGreen, upperStripBlue);
+      } else {
+          upperStripGreen = (int)value*2;
+      }
+  } else if (controlType == 25) {
+      if(noteReceived == 46) {
+        upperStripBlue = (int)value*2;
+        upperStripOn(upperStripRed, upperStripGreen, upperStripBlue);
+      } else {
+          upperStripBlue = (int)value*2;
+      }
   }
 }
 
@@ -147,55 +122,6 @@ void stripsOn(int red, int green, int blue) {
   lowerStripOn(red, green, blue);
   upperStripOn(red, green, blue);
 }
-
-void stripsOff() {
-  lowerStripOn(0, 0, 0);
-  upperStripOn(0, 0, 0);
-  lowerStrip.show();
-  upperStrip.show();
-}
-
-void panOut(int red, int green, int blue, int control) {
-  //Go through the loop 'control' amount of times
-  //Loop? Needs to increment and decrement at the same time
-  //Two variables- the more 'control' the more LEDs light up
-  //72 leds in Strip -> always minus 1 when coding
-  //middle LEDs are 35+36 with -1
-  //can't compare LED position with control- control needs to be relative
- 
-  // control = (35/127)*control;
-  // Serial.write("Coverted control is " + control);
-  // Serial.write("Last control is " + lastCC);
-
-
-  
-  if(!off) {  
-
-    int starting = 35 - control;
-    int ending = control + 36;
-
-    if (starting < 0) {
-      starting = 0;
-    }
-
-    if (ending > 71) {
-      ending = 71;
-    }
-
-    //led less than needs to be 35
-    //need to subtrac number off from 35
-
-    for (int i=35;i>starting;i--) {
-      lowerStrip.setPixelColor(i, lowerStrip.Color(red, green, blue));
-    }
-    for (int i=36;i<ending;i++) {
-      lowerStrip.setPixelColor(i, lowerStrip.Color(red, green, blue));
-    }
-
-    lowerStrip.show();
-  }
-}
-
 
 void lowerStripFirst(int red, int green, int blue) {
   for (int i=0;i<11;i++) {
@@ -209,9 +135,8 @@ void lowerStripFirst(int red, int green, int blue) {
   lowerStripBlue = blue;
 }
 
-<<<<<<< HEAD
 void lowerStripSecond(int red, int green, int blue) {
-  for (int i=11;i<22;i++) {
+  for (int i=12;i<23;i++) {
     lowerStrip.setPixelColor(i, lowerStrip.Color(red, green, blue));
   }
 
@@ -222,20 +147,6 @@ void lowerStripSecond(int red, int green, int blue) {
   lowerStripBlue = blue;
 }
 
-void lowerStripThird(int red, int green, int blue) {
-  for (int i=22;i<33;i++) {
-    lowerStrip.setPixelColor(i, lowerStrip.Color(red, green, blue));
-  }
-
-  lowerStrip.show();
-
-  lowerStripRed = red;
-  lowerStripGreen = green;
-  lowerStripBlue = blue;
-}
-
-=======
->>>>>>> parent of 2988218... Pitch commit
 void lowerStripOn(int red, int green, int blue) {
   
   for(int i=0;i<LOWER_STRIP_PIXELS;i++) {
@@ -261,6 +172,65 @@ void upperStripOn(int red, int green, int blue) {
   upperStripGreen = green;
   upperStripBlue = blue;
 }
+int lastCC = 0;
+void upAndDown(int red, int green, int blue, int ccValue) {
+  lowerStrip.setPixelColor(lastCC, lowerStrip.Color(0, 0, 0));
+  lowerStrip.show();
+  if(ccValue < 72)
+  {
+      lowerStrip.setPixelColor(ccValue, lowerStrip.Color(red, green, blue));
+      lowerStrip.show();
+  }
+  lastCC = ccValue;
+
+}
+
+//change color with x axis (cc what?)
+//change brightness with y axis (cc what?)
+
+//need to write fade animation 
+//correlate center led out with data from xy pad
+// void lowerStripExpand(int red, int green, int blue, int ccValue) {
+
+// //mess with delay knoh for actual expand
+//   // 72 pixels (0-71)
+//   // start at 36
+
+//   // cc value 1 ->>> led 36
+//   // cc value 2 ->>> led 35, 36, 37
+//   // cc value 3 ->>> led 34, 34, 35, 36, 37
+
+//   int forward = 36;
+//   for(int i=36; i<(ccValue/2);i--)
+//   {
+//     lowerStrip.setPixelColor(i, lowerStrip.Color(red, green, blue));
+//     forward++;
+//     lowerStrip.setPixelColor(forward, lowerStrip.Color(red, green, blue));
+//     lowerStrip.show();
+//   }  
+
+//   int forward = 36;
+//   for(int i=36; i<LOWER_STRIP_PIXELS;i++)
+//   {
+//     //get the cc value
+//     //if cc value is withing
+//     int ledPosition = i;
+//     lowerStrip.setPixelColor(i, lowerStrip.Color(red, green, blue));
+//     lowerStrip.setPixelColor(i, lowerStrip.Color(red, green, blue));
+//     forward++;
+//     lowerStrip.setPixelColor(forward, lowerStrip.Color(red, green, blue));
+//     lowerStrip.show();
+//   }
+
+// }
+
+// void lowerStripScroll(int red int green, int blue, int ccValue) {
+//     for(int i=0;i<ccValue*2;i++) {
+//       lowerStrip.setPixelColor(i, lowerStrip.Color(red, green, blue)); 
+//   } 
+  
+//   lowerStrip.show();
+// }
 
 void clearStrips() {
   clearLowerStrip();
